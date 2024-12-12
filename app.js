4de9b548ec6115d86a83eedff44bf556bf4cc5d1
Kundan72 <kundankrdas22@gmail.com>
2024-12-13 00:04:10 +0530

    adding files

--- app.js
+++ app.js
@@ -12,29 +12,3 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-
-
-// EmailJs
-
-// document.getElementById('contact-form').addEventListener('submit', function(event) {
-//     event.preventDefault();
-
-//     emailjs.sendForm('service_59hfxla', 'template_e25xjmy', this)
-//         .then(function(response) {
-//             console.log('SUCCESS!', response.status, response.text);
-//             Swal.fire({
-//                 title: 'Success!',
-//                 text: 'Your message has been sent.',
-//                 icon: 'success',
-//                 confirmButtonText: 'OK'
-//             });
-//         }, function(error) {
-//             console.log('FAILED...', error);
-//             Swal.fire({
-//                 title: 'Error!',
-//                 text: 'There was an error sending your message. Please try again later.',
-//                 icon: 'error',
-//                 confirmButtonText: 'OK'
-//             });
-//         });
-// });
